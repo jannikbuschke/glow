b725315262d0d66f35f7e2b599e5423081cad440
Jannik Buschke <jannik.buschke@gmail.com>
2020-02-22 11:28:58 +0100

    add ForbiddenException

--- BadRequestException.cs
+++ BadRequestException.cs
@@ -6,4 +6,9 @@ namespace JannikB.Glue.AspNetCore
     {
         public BadRequestException(string msg) : base(msg) { }
     }
+
+    public class ForbiddenException: Exception
+    {
+        public ForbiddenException(string msg) : base(msg) { }
+    }
 }
