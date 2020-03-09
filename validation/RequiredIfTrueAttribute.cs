6f16c2636741344d14a9c45fce5646548a9f7f3d
Jannik Buschke <jannik.buschke@gmail.com>
2020-03-09 19:49:41 +0100

    feat: implement RequiredIfTruAttribute

---
+++ validation/RequiredIfTrueAttribute.cs
@@ -0,0 +1,31 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace JannikB.Glue
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class RequiredIfTrueAttribute : RequiredAttribute
+    {
+        private string PropertyName { get; set; }
+
+        public RequiredIfTrueAttribute(string propertyName)
+        {
+            PropertyName = propertyName;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext context)
+        {
+            var instance = context.ObjectInstance;
+            Type type = instance.GetType();
+
+            bool.TryParse(type.GetProperty(PropertyName).GetValue(instance)?.ToString(), out var propertyValue);
+
+            if (propertyValue && string.IsNullOrWhiteSpace(value?.ToString()))
+            {
+                return new ValidationResult(ErrorMessage);
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
