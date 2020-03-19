fa8f7b0e7907d19ef6bbf810efd24362d21005db
Jannik Buschke <jannik.buschke@gmail.com>
2020-03-19 11:11:24 +0100

    minor

--- Guids.cs
+++ Guids.cs
@@ -4,7 +4,9 @@ namespace JannikB.Glue.AspNetCore
 {
     public static class Guids
     {
-        public static readonly Guid ONE = Guid.Parse("00000000-0000-0000-0000-000000000001");
-        public static readonly Guid TWO = Guid.Parse("00000000-0000-0000-0000-000000000002");
+        public const string GuidOne = "00000000-0000-0000-0000-000000000001";
+        public const string GuidTwo = "00000000-0000-0000-0000-000000000002";
+        public static readonly Guid ONE = Guid.Parse(GuidOne);
+        public static readonly Guid TWO = Guid.Parse(GuidTwo);
     }
 }
