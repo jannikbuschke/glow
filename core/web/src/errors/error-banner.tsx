0d1e72dbba5bb764ee5745b80a0a7002408f8c68
Jannik Buschke <jannik.buschke@gmail.com>
2020-05-17 14:48:19 +0200

    add Banners, adjust border

--- core/web/src/errors/error-banner.tsx
+++ core/web/src/errors/error-banner.tsx
@@ -1,13 +1,37 @@
 import * as React from "react"
 import { Alert } from "antd"
 
+export function WarningBanner({ message }: { message: any }) {
+  return render("warning", message)
+}
 export function ErrorBanner({ error }: { error: any }) {
-  return error ? (
+  return render("error", error)
+}
+
+export function InfoBanner({ message }: { message: any }) {
+  return render("info", message)
+}
+export function SuccessBanner({ message }: { message: any }) {
+  return render("success", message)
+}
+
+function render(
+  type: "error" | "info" | "warning" | "success",
+  msg: string | null | undefined,
+) {
+  return msg ? (
     <Alert
-      type="error"
-      banner={true}
-      message={error.toString()}
+      type={type}
+      message={msg.toString()}
       showIcon={false}
+      style={{
+        borderRight: "none",
+        borderBottom: "none",
+        borderTop: "none",
+        borderLeftWidth: 5,
+        marginTop: 5,
+        marginBottom: 5,
+      }}
     />
   ) : null
 }
