d30bfce3657db23973b4231f03abc2bef124be13
Jannik Buschke <jannik.buschke@gmail.com>
2020-05-24 22:31:09 +0200

    wip

--- sample/web/src/App.tsx
+++ sample/web/src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import logo from "./logo.svg"
 import "./App.css"
-<<<<<<< HEAD
 import { Tabs } from "antd"
 import { ApplicationLayout } from "glow-react/es/Layout/Layout"
 import "antd/dist/antd.css"
@@ -20,45 +19,6 @@ function App() {
         </Tabs>
       </ApplicationLayout>
     </Router>
-=======
-import { Formik } from "formik"
-import { Input, SubmitButton, Form } from "formik-antd"
-import { notification } from "antd"
-import { AntDesignOutlined } from "@ant-design/icons"
-import { ApplicationLayout } from "glow-react/es/Layout/Layout"
-import "antd/dist/antd.css"
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-
-      <div style={{ background: "white", padding: "20px" }}>
-        <Formik
-          initialValues={{ field1: "hello world" }}
-          onSubmit={(values, f) => {
-            notification.info({ message: values.field1 })
-            f.setSubmitting(false)
-          }}
-        >
-          <Form>
-            <Input name="field1" />
-            <SubmitButton
-              style={{ marginTop: 10 }}
-              icon={<AntDesignOutlined />}
-            >
-              Submit
-            </SubmitButton>
-          </Form>
-        </Formik>
-      </div>
-      <ApplicationLayout Header={<div>HEADER</div>}>
-        <div>chilcren</div>
-      </ApplicationLayout>
-    </div>
->>>>>>> master
   )
 }
 
