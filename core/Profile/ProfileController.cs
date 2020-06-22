6f013e99b32dbedfec0f3b0ed395449f95376c2e
Jannik Buschke <jannik.buschke@gmail.com>
2020-06-22 15:41:46 +0200

    cleanup

--- core/Profile/ProfileController.cs
+++ core/Profile/ProfileController.cs
@@ -1,6 +1,4 @@
-using System.Collections.Generic;
 using System.Linq;
-using System.Security.Claims;
 using JannikB.Glue.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
