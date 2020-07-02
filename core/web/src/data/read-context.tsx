8fc8a051ce16b17a1acbd3c1e439d49e35a02ef8
Jannik Buschke <jannik.buschke@gmail.com>
2020-07-02 11:56:51 +0200

    feat (ReadContext): add optional reload function

--- core/web/src/data/read-context.tsx
+++ core/web/src/data/read-context.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 
 interface IReadContext<T = any> {
   value: T
+  reload: () => void
 }
 
 const ReadContext = React.createContext<IReadContext>(undefined as any)
@@ -15,10 +16,16 @@ export function useReadContext<T>() {
 }
 
 export function ReadContextProvider<T>({
+  reload,
   value,
   children,
-}: React.PropsWithChildren<{ value: T }>) {
+}: React.PropsWithChildren<{ value: T; reload?: () => void }>) {
+  const reloadValue = () => {
+    console.warn("reload not implemented by this ReadContextProvider")
+  }
   return (
-    <ReadContext.Provider value={{ value }}>{children}</ReadContext.Provider>
+    <ReadContext.Provider value={{ value, reload: reload || reloadValue }}>
+      {children}
+    </ReadContext.Provider>
   )
 }
