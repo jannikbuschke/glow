3b2d087189f53b287a15db628c7effdcaf199230
Jannik Buschke <jannik.buschke@gmail.com>
2020-10-06 12:42:47 +0200

    feat (useData): allow adding react-query options

--- core/Typescript/RequestDescription.cs
+++ core/Typescript/RequestDescription.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using Microsoft.Extensions.Hosting;
 
 namespace Glow.TypeScript
 {
--- core/web/src/query/use-data.ts
+++ core/web/src/query/use-data.ts
@@ -1,10 +1,15 @@
 import * as React from "react"
-import { useQuery } from "react-query"
+import { QueryOptions, useQuery } from "react-query"
 import { fetchJson } from "../http/fetch"
 
-export function useData<T>(url: string, placeholder: T) {
+export function useData<T>(
+  url: string,
+  placeholder: T,
+  config?: QueryOptions<T>,
+) {
   const { data, ...rest } = useQuery<T, any>(url, () => fetchJson(url), {
     retry: 1,
+    ...config,
   })
   if (!Boolean(data)) {
     return {
