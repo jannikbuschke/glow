c08b98688f6eb5ce1789d0809f5d55e20ee3ad46
Jannik Buschke <jannik.buschke@gmail.com>
2020-11-05 22:23:01 +0100

    remove package-lock.json

--- core/ServiceExtensions.cs
+++ core/ServiceExtensions.cs
@@ -6,6 +6,11 @@ namespace Glow.Core
 {
     public static class ServiceExtensions
     {
+        /// <summary>
+        /// Adds FileService, MockExternalSystems and Clock
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
         public static IServiceCollection AddGlow(this IServiceCollection services)
         {
             services.AddSingleton<FileService>();
