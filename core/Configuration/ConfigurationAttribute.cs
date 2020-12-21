2dfdd67c3d719581923e7b8195c41327af76be4f
Jannik Buschke <jbu@janniks.work>
2020-12-21 19:45:16 +0100

    minor (Configuration): deprecat Path property

--- core/Configuration/ConfigurationAttribute.cs
+++ core/Configuration/ConfigurationAttribute.cs
@@ -5,14 +5,23 @@ namespace Glow.Configurations
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
     public class ConfigurationAttribute : Attribute, IConfigurationMeta
     {
+        /// <summary>
+        /// Required. Unique identifier
+        /// </summary>
         public string Id { get; set; }
         public string Policy { get; set; }
         public string ReadPolicy { get; set; }
         public string SectionId { get; set; }
         private string path;
+        /// <summary>
+        /// (Optional) Part of the path. The fully qualified path will be
+        /// api/configurations/:path
+        /// If Path is not set, Id will be used
+        /// </summary>
         public string Path
         {
             get { return path ?? Id; }
+            [Obsolete("Use Id instead")]
             set { path = value; }
         }
         public string Route
