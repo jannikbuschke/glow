4709bae3c229aabecfaf8c0a3d7b9db9971a51d3
Jannik Buschke <jbu@janniks.work>
2020-12-25 23:02:42 +0100

    remove obsolet code and odata

--- core/Odata/OdataPage.cs
+++
@@ -1,7 +0,0 @@
-namespace JannikB.Glue.AspNetCore.Odata
-{
-    public class OdataPage<T>
-    {
-        public T[] Value { get; set; }
-    }
-}
--- core/deprecated/AuthenticationSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace JannikB.Glue.AspNetCore
-{
-    public class AuthenticationSettings
-    {
-        public string ClientId { get; set; }
-        public string TenantId { get; set; }
-    }
-}
