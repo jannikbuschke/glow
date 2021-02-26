4720fa540db8218e5e08011e790885e4176afd40
Jannik Buschke <jbu@janniks.work>
2021-02-26 12:32:04 +0100

    add ActionButton

---
+++ core/web/src/Forms/action-button.tsx
@@ -0,0 +1,30 @@
+import Button, { ButtonProps } from "antd/lib/button"
+import * as React from "react"
+import { notifyError } from "../errors/error-banner"
+import { Result, useSubmit } from "./use-submit"
+
+export type ActionButtonProps<T> = {
+  url: string
+  payload: T
+  onResponse?: (result: Result<T>) => void
+} & Omit<ButtonProps, "onClick" | "loading">
+
+export function ActionButton<T = any>(props: ActionButtonProps<T>) {
+  const { url, payload, onResponse, ...buttonProps } = props
+  const [submit, validate, { error, submitting }] = useSubmit(url)
+
+  return (
+    <Button
+      {...buttonProps}
+      loading={submitting}
+      onClick={async () => {
+        const response = await submit(payload)
+        if (onResponse) {
+          onResponse(response)
+        } else if (!response.ok) {
+          notifyError(response.error)
+        }
+      }}
+    />
+  )
+}
