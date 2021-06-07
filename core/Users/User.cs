61b37c05e2b8c62e369b876746572cbf3b5c62f8
Jannik Buschke <jannik.buschke@gmail.com>
2021-06-07 12:29:11 +0200

    add User

---
+++ core/Users/User.cs
@@ -0,0 +1,42 @@
+using System.Threading.Tasks;
+using Glow.Core.Actions;
+using Glow.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gertrud.Users
+{
+    public class User : IUser
+    {
+        public const string SystemUserId = "___system___";
+        public string Id { get; set; }
+        public string TenantId { get; set; }
+        public string DisplayName { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public UserDto ToDto()
+        {
+            return new UserDto
+            {
+                Id = Id,
+                DisplayName = DisplayName,
+                Email = Email
+
+            };
+        }
+    }
+
+    public static class GraphUserExtension
+    {
+        public static User ToUser(this Microsoft.Graph.User user)
+        {
+            return new User
+            {
+                Id = user.Id,
+                DisplayName = user.DisplayName,
+                Email = user.Mail
+            };
+        }
+    }
+}
