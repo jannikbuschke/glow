dd3ec7c90fa48a4fb230ecca3c101ed84d3e64b8
Guilherme Rodrigues <gro@phatconsulting.de>
2021-06-15 10:26:20 +0200

    refactor: rename ServiceUserConfiguration.SectionId

--- core/Notifications/ServiceUserConfiguration.cs
+++ core/Notifications/ServiceUserConfiguration.cs
@@ -2,7 +2,7 @@ namespace Glow
 {
     public class ServiceUserConfiguration
     {
-        public const string ConfigPath = "ServiceUser";
+        public const string SectionId = "ServiceUser";
         public string Username { get; set; }
         public string Password { get; set; }
     }
