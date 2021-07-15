e3a0d827993b0b2b4263a696609b1d3790cbec03
Jannik Buschke <jbu@janniks.work>
2021-07-15 21:49:05 +0200

    fix (translations): minor

--- core/web/src/i18n/fetch-translations.ts
+++ core/web/src/i18n/fetch-translations.ts
@@ -9,9 +9,14 @@ interface Translations {
 export async function fetchTranslations() {
   const response = await fetch("/languages/translations.json")
   if (response.ok) {
-    const data = await response.json()
-    console.log({ translations: data })
-    return data as Translations
+    try {
+      const data = await response.json()
+      console.log({ translations: data })
+      return data as Translations
+    } catch (e) {
+      console.warn("could not load additional translations", { error: e })
+      return null
+    }
   } else {
     const error = await response.text()
     console.warn("could not load additional translations", { error })
