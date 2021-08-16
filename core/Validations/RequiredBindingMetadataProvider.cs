623053798301bbc6f530473ac56e14d850bd146e
Jannik Buschke <jbu@janniks.work>
2021-08-16 10:57:09 +0200

    add RequiredBindingMetadataProvider

---
+++ core/Validations/RequiredBindingMetadataProvider.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
+
+namespace Glow.Validation
+{
+    public class RequiredBindingMetadataProvider : IBindingMetadataProvider
+    {
+        // public void GetBindingMetadata(BindingMetadataProviderContext context)
+        // {
+        //     if (context.PropertyAttributes.OfType<RequiredAttribute>().Any())
+        //     {
+        //         context.BindingMetadata.IsBindingRequired = true;
+        //     }
+        // }
+
+        public void CreateBindingMetadata(BindingMetadataProviderContext context)
+        {
+            if (context.PropertyAttributes.OfType<RequiredAttribute>().Any())
+            {
+                context.BindingMetadata.IsBindingRequired = true;
+            }
+        }
+    }
+}
