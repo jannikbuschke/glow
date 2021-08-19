ffc72a0092e9f7098d35d9f1de5f1da1311b96e9
Jannik Buschke <jbu@janniks.work>
2021-08-19 16:30:15 +0200

    feat (Configuration): add GetAppBaseUrl (OpenIdConnect:BaseUrl)

---
+++ core/Configuration/IConfigurationExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Glow.Configurations
+{
+    public static class IConfigurationExtensions
+    {
+        public static string GetAppBaseUrl(this IConfiguration configuration)
+        {
+            return configuration.GetValue<string>("OpenIdConnect:BaseUrl");
+        }
+    }
+}
