7b252834151128df33bdb110bc78438c0b59a119
Jannik Buschke <jbu@janniks.work>
2021-08-25 21:36:02 +0200

    add BackgroundActionIdentifier

---
+++ core/Notifications/BackgroundActionIdentifier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Glow.Core.Notifications
+{
+    public class BackgroundActionIdentifier<T>
+    {
+        public string ActionName { get; }
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string FullId()
+        {
+            return $"{ActionName}:{Id.ToString()}";
+        }
+
+        public BackgroundActionIdentifier()
+        {
+            ActionName = typeof(T).FullName;
+        }
+    }
+}
