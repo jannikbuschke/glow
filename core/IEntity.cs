46ef7fe2547864b3e0f9ffb842df02fcca3504e9
Jannik Buschke <jbu@janniks.work>
2021-09-01 15:33:14 +0200

    feat (ITrackEdits): add ITrackEdits interface

--- core/IEntity.cs
+++ core/IEntity.cs
@@ -1,4 +1,10 @@
 using System;
+using Gertrud.Users;
+using Glow.Clocks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Glow.AspNetCore.Utils.Module
 {
@@ -8,11 +14,44 @@ namespace Glow.AspNetCore.Utils.Module
     {
         T Id { get; }
 
-        public string DisplayName { get; set; }
+        string DisplayName { get; set; }
     }
 
     public interface IAuditLogDisplayName
     {
         string DisplayName { get; }
+    }
+
+    public interface ITrackEdits
+    {
+        string CreatedBy { get; set; }
+        DateTime? CreatedAt { get; set; }
+        string UpdatedBy { get; set; }
+        DateTime? UpdatedAt { get; set; }
+    }
+
+    public static class DataContextExtension
+    {
+        public static void HandleITrackEdits(this DbContext self, IServiceProvider services, string systemUserId = User.SystemUserId)
+        {
+            IHttpContextAccessor httpContextAccessor = services.GetRequiredService<IHttpContextAccessor>();
+            var userId = httpContextAccessor?.HttpContext?.GetUserObjectId() ?? systemUserId;
+            IClock clock = services.GetRequiredService<IClock>();
+
+            foreach (EntityEntry<ITrackEdits> v in self.ChangeTracker.Entries<ITrackEdits>())
+            {
+                if (v.State == EntityState.Added)
+                {
+                    v.Entity.CreatedAt ??= clock.Now;
+                    v.Entity.CreatedBy ??= userId;
+                }
+
+                if (v.State == EntityState.Modified)
+                {
+                    v.Entity.UpdatedAt = clock.Now;
+                    v.Entity.UpdatedBy = userId;
+                }
+            }
+        }
     }
 }
