b8111700088aa4494c3234b2823921a9d59c1b57
Jannik Buschke <jbu@janniks.work>
2021-09-03 13:21:35 +0200

    add default policies admin and authenticated user

---
+++ core/Authentication/Defaults.cs
@@ -0,0 +1,8 @@
+﻿namespace Glow.Core.Authentication
+{
+    public class DefaultPolicies
+    {
+        public const string Admin = "Admin";
+        public const string AuthenticatedUser = "AuthenticatedUser";
+    }
+}
