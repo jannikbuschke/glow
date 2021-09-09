6b0b2f9b1fd04284b3bf19b5b9b22193fb28e1c1
Jannik Buschke <jbu@janniks.work>
2021-09-09 10:06:29 +0200

    feat (ListExtensions): add AddIfNotEmpty

---
+++ core/Helpers/ListExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Glow.Core.Helpers
+{
+    public static class ListExtensions
+    {
+        public static void AddIfNotEmptyOrNull(this IList<string> self, string value)
+        {
+            if (!string.IsNullOrEmpty(value))
+            {
+                self.Add(value);
+            }
+        }
+
+        public static void AddIfNotNull<T>(this IList<T> self, T value)
+        {
+            if (value != null)
+            {
+                self.Add(value);
+            }
+        }
+    }
+}
