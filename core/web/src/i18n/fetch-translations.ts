935045d7739587fb2753baaab36b054b8e763500
Guilherme Rodrigues <gro@phatconsulting.de>
2021-09-20 11:47:11 +0200

    fetch-translations.ts update - fetchTranslationCollection( ) added

--- core/web/src/i18n/fetch-translations.ts
+++ core/web/src/i18n/fetch-translations.ts
@@ -23,3 +23,41 @@ export async function fetchTranslations() {
     return null
   }
 }
+
+interface TranslationCollection {
+  id: string
+  values: [
+    {
+      languageId: string
+      displayName: string
+      basedOn: string
+      resources: { [key: string]: string }
+    },
+  ]
+}
+
+export async function fetchTranslationCollection() {
+  const response = await fetch("/api/translations/get", {
+    method: "POST",
+    body: JSON.stringify({}),
+    credentials: "same-origin",
+    headers: {
+      "x-submit-intent": "execute",
+      "content-type": "application/json",
+    },
+  })
+  if (response.ok) {
+    try {
+      const data = await response.json()
+      console.log({ translations: data })
+      return data as TranslationCollection
+    } catch (e) {
+      console.warn("could not load additional translations", { error: e })
+      return null
+    }
+  } else {
+    const error = await response.text()
+    console.warn("could not load additional translations", { error })
+    return null
+  }
+}
