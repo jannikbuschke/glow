26d36fcd43536a03b45732802bdfe6edcdb67f0f
Guilherme Rodrigues <gro@phatconsulting.de>
2021-09-27 12:29:22 +0200

    minor

--- core/Typescript/GetTypeExtension.cs
+++ core/Typescript/GetTypeExtension.cs
@@ -131,6 +131,8 @@ namespace Glow.Core.Typescript
                 {typeof(IEnumerable<double>), new Tuple<string, string>("number[]", "[]")},
                 {typeof(Collection<string>), new Tuple<string, string>("(string | null)[]", "[]")},
                 {typeof(ICollection<string>), new Tuple<string, string>("(string | null)[]", "[]")},
+                {typeof(TimeSpan), new Tuple<string, string>("string","null")},
+                {typeof(TimeSpan?), new Tuple<string, string>("string | null","null")},
             };
     }
 }
