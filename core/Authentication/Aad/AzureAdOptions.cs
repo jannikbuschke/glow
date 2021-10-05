d038c68bd44aa88b9d71fdd0057b1ee06348f328
Jannik Buschke <jbu@janniks.work>
2021-10-05 23:32:09 +0200

    set AzureAdOptions Instance and Callback defaults

--- core/Authentication/Aad/AzureAdOptions.cs
+++ core/Authentication/Aad/AzureAdOptions.cs
@@ -4,10 +4,10 @@ namespace Glow.Authentication.Aad
     {
         public string ClientId { get; set; }
         public string ClientSecret { get; set; }
-        public string Instance { get; set; }
+        public string Instance { get; set; } = "https://login.microsoftonline.com/";
         public string Domain { get; set; }
         public string TenantId { get; set; }
-        public string CallbackPath { get; set; }
+        public string CallbackPath { get; set; } = "/signin-oidc";
         public string BaseUrl { get; set; }
 
         public string[] ValidIssuers { get; set; }
