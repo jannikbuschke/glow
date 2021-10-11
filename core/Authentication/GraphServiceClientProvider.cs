5b65405786f790725d0d683b1cf3bd8911931efb
Jannik Buschke <jbu@janniks.work>
2021-10-11 14:37:56 +0200

    Add GraphserviceClientProvider.CreateClient(string token, bool useBeta)

---
+++ core/Authentication/GraphServiceClientProvider.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.Graph;
+
+namespace Glow.Core.Authentication
+{
+    public static class GraphServiceClientProvider
+    {
+        public static GraphServiceClient CreateGetClient(string token, bool useBeta = false)
+        {
+            var client = new GraphServiceClient(
+                useBeta ? "https://graph.microsoft.com/beta" : "https://graph.microsoft.com/v1.0/",
+                new DelegateAuthenticationProvider(
+                    (requestMessage) =>
+                    {
+                        requestMessage.Headers.Authorization = new AuthenticationHeaderValue("bearer", token);
+                        return Task.FromResult(requestMessage);
+                    }
+                ));
+            return client;
+        }
+    }
+}
