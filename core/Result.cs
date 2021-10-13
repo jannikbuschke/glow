bf363a1606b99d574fbc0ea825a837d2d35df93e
Jannik Buschke <jbu@janniks.work>
2021-10-13 16:24:43 +0200

    feat (Result): add Result<T> class

---
+++ core/Result.cs
@@ -0,0 +1,19 @@
+﻿namespace Glow.Core
+{
+    public class Result<T>
+    {
+        public bool IsSuccessful { get; set; }
+        public T Payload { get; set; }
+        public string ErrorMessage { get; set; }
+
+        public static Result<T> Success(T payload)
+        {
+            return new() { IsSuccessful = true, Payload = payload };
+        }
+
+        public static Result<T> Fail(string errorMessage)
+        {
+            return new() { IsSuccessful = false, ErrorMessage = errorMessage };
+        }
+    }
+}
