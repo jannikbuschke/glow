4c77db1cbe06df327f4178c10fe922983ab0b625
Jannik Buschke <jbu@janniks.work>
2021-10-22 10:22:44 +0200

    fix SitsReadWriteAll scope

--- core/MsGraph/Scopes.cs
+++ core/MsGraph/Scopes.cs
@@ -3,6 +3,6 @@ namespace Glow.MsGraph
     public static class Scopes
     {
         public const string MailReadWriteShared = "mail.readwrite.shared";
-        public const string SitesReadWriteShared = "sites.readwrite.shared";
+        public const string SitesReadWriteAll = "sites.readwrite.all";
     }
 }
