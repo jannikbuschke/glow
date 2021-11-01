9143870e9f15ed471a1ea0fa0514ddd1695c2a88
Jannik Buschke <jbu@janniks.work>
2021-11-01 12:34:59 +0100

    refactor (ModalContainer): refactor ModalContainer with triggerType 'Button' and 'MenuItem'

--- core/web/src/buttons/modal-button-container.tsx
+++ core/web/src/buttons/modal-button-container.tsx
@@ -10,29 +10,50 @@ type PropsButton = Omit<ButtonProps, "onClick"> & Props
 
 type PropsMenuItem = Omit<MenuItemProps, "onClick"> & Props
 
+type PropsButtonV2 = { triggerType: "Button" } & Omit<ButtonProps, "onClick">
+
+type PropsMenuItemV2 = { triggerType: "MenuItem" } & Omit<
+  MenuItemProps,
+  "onClick"
+>
+
+export type ModalTriggerType = "Button" | "MenuItem"
+
 export type AutoControlledModalProps = {
   visible: boolean
   onCancel: () => void
   afterClose: () => void
 }
 
-export function ModalMenuItemContainer({
+export function ModalContainer({
   modal,
   unmountModalOnAfterClose,
+  triggerType,
   ...props
-}: PropsMenuItem) {
+}: (PropsMenuItemV2 | PropsButtonV2) & Props) {
+  const btnProps = props as PropsButtonV2
+  const menuItemProps = props as PropsMenuItemV2
   const [visible, setVisible] = React.useState(false)
   const [mountModal, setMountModal] = React.useState(false)
-
   return (
     <>
-      <Menu.Item
-        {...props}
-        onClick={() => {
-          setVisible(true)
-          setMountModal(true)
-        }}
-      />
+      {triggerType === "MenuItem" ? (
+        <Menu.Item
+          {...menuItemProps}
+          onClick={() => {
+            setVisible(true)
+            setMountModal(true)
+          }}
+        />
+      ) : (
+        <Button
+          {...btnProps}
+          onClick={() => {
+            setVisible(true)
+            setMountModal(true)
+          }}
+        />
+      )}
       {unmountModalOnAfterClose && !mountModal
         ? null
         : React.cloneElement(modal, {
@@ -48,36 +69,34 @@ export function ModalMenuItemContainer({
   )
 }
 
+/// deprecated, use ModalContainer
+export function ModalMenuItemContainer({
+  modal,
+  unmountModalOnAfterClose,
+  ...props
+}: PropsMenuItem) {
+  return (
+    <ModalContainer
+      triggerType="MenuItem"
+      modal={modal}
+      unmountModalOnAfterClose={unmountModalOnAfterClose}
+      {...props}
+    />
+  )
+}
+
+/// deprecated, use ModalContainer
 export function ModalButtonContainer({
   modal,
   unmountModalOnAfterClose,
   ...props
 }: PropsButton) {
-  const [visible, setVisible] = React.useState(false)
-  const [mountModal, setMountModal] = React.useState(false)
-
   return (
-    <>
-      <Button
-        {...props}
-        onClick={() => {
-          setVisible(true)
-          setMountModal(true)
-        }}
-      />
-      {/* if unmountModalOnAfterClose is undefined, modal will be mounted in
-      unvisible state */}
-      {unmountModalOnAfterClose && !mountModal
-        ? null
-        : React.cloneElement(modal, {
-            visible,
-            onCancel: () => {
-              setVisible(false)
-            },
-            afterClose: () => {
-              setMountModal(false)
-            },
-          } as AutoControlledModalProps)}
-    </>
+    <ModalContainer
+      triggerType="Button"
+      modal={modal}
+      unmountModalOnAfterClose={unmountModalOnAfterClose}
+      {...props}
+    />
   )
 }
