9eba2f91376ba00f7bf655e8bf70adb68ab1e2bc
Jannik Buschke <jbu@janniks.work>
2021-11-02 19:32:25 +0100

    feat (Validation): add RequiredIfNull validation attribute

---
+++ core/Validations/RequiredIfNull.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Glow.Validation
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class RequiredIfNull : RequiredAttribute
+    {
+        private string[] PropertyName { get; set; }
+
+        public RequiredIfNull(params string[] properties)
+        {
+            PropertyName = properties;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext context)
+        {
+            var instance = context.ObjectInstance;
+            Type type = instance.GetType();
+
+            var valueString = value?.ToString();
+            foreach (var v in PropertyName)
+            {
+                if (type.GetProperty(v).GetValue(instance) == null && value == null)
+                {
+                    return new ValidationResult(ErrorMessage);
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
