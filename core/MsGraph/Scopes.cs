2265d0bec4ec2d0b8be8c08ce69938665608ffed
Jannik Buschke <jbu@janniks.work>
2021-11-08 10:46:57 +0100

    add Scopes.MailSendShared

--- core/MsGraph/Scopes.cs
+++ core/MsGraph/Scopes.cs
@@ -3,6 +3,7 @@ namespace Glow.MsGraph
     public static class Scopes
     {
         public const string MailReadWriteShared = "mail.readwrite.shared";
+        public const string MailSendShared = "mail.send.shared";
         public const string SitesReadWriteAll = "sites.readwrite.all";
     }
 }
