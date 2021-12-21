24ff6692f8dcafd32c3f9b2cdc2ccec600ec087b
Jannik Buschke <jbu@janniks.work>
2021-12-21 15:57:25 +0100

    feat (Scopes): add calendars.readwrite.shared

--- core/MsGraph/Scopes.cs
+++ core/MsGraph/Scopes.cs
@@ -5,5 +5,6 @@ namespace Glow.MsGraph
         public const string MailReadWriteShared = "mail.readwrite.shared";
         public const string MailSendShared = "mail.send.shared";
         public const string SitesReadWriteAll = "sites.readwrite.all";
+        public const string CalendarsReadWriteShared = "calendars.readwrite.shared";
     }
 }
