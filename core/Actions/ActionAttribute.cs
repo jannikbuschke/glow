c7ac1a67cfb5bb3cdf2f7a34b28852316e7bd957
Jannik Buschke <jbu@janniks.work>
2021-12-29 08:30:53 +0100

    rename AlloyAnonymous attribute

--- core/Actions/ActionAttribute.cs
+++ core/Actions/ActionAttribute.cs
@@ -8,7 +8,7 @@ namespace Glow.Core.Actions
     {
         public string Route { get; set; }
         public string Policy { get; set; }
-        public bool AllowAnoymous { get; set; }
+        public bool AllowAnonymous { get; set; }
     }
 
     public class ActionMeta
