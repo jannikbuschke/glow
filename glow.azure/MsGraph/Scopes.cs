9dd03a44a84efa829b277661db11e2fcda0f669e
Jannik Buschke <jbu@janniks.work>
2022-03-07 14:54:38 +0100

    feat (Scopes): add some well known scopes

--- glow.azure/MsGraph/Scopes.cs
+++ glow.azure/MsGraph/Scopes.cs
@@ -2,9 +2,16 @@ namespace Glow.MsGraph
 {
     public static class Scopes
     {
+        public const string UserReadBasicAll = "user.readbasic.all";
+
         public const string MailReadWriteShared = "mail.readwrite.shared";
         public const string MailSendShared = "mail.send.shared";
         public const string SitesReadWriteAll = "sites.readwrite.all";
         public const string CalendarsReadWriteShared = "calendars.readwrite.shared";
+
+
+        public const string TeamReadBasicAll = "Team.ReadBasic.All";
+        public const string  ChannelReadBasicAll = "Channel.ReadBasic.All";
+        public const string ChannelMessageSend = "ChannelMessage.Send";
     }
 }
