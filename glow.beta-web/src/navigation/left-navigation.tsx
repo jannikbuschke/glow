665dfa57e516e646563183d6e6d805b8881f5fb1
Jannik Buschke <jbu@janniks.work>
2022-03-21 18:39:12 +0100

    feat (LeftNavigation): add support for children, make icons optional

--- glow.beta-web/src/navigation/left-navigation.tsx
+++ glow.beta-web/src/navigation/left-navigation.tsx
@@ -4,8 +4,15 @@ import { Layout, Menu } from "antd"
 import { Route, Routes } from "react-router-dom"
 import { Link } from "react-router-dom"
 
+type Item = {
+  key: string
+  content: React.ReactNode
+  icon?: React.ReactNode
+  children?: Item[]
+}
+
 interface LeftNavigationProps {
-  items: { key: string; content: React.ReactNode; icon: React.ReactNode }[]
+  items: Item[]
 }
 
 const { Sider } = Layout
@@ -36,11 +43,21 @@ export function _LeftNavigation({ items }: LeftNavigationProps) {
         mode="inline"
         // style={{ width: 200 }}
       >
-        {items.map((v) => (
-          <Menu.Item key={v.key} icon={v.icon}>
-            <Link to={`/${v.key}`}>{v.content}</Link>
-          </Menu.Item>
-        ))}
+        {items.map((v) =>
+          v.children ? (
+            <Menu.SubMenu icon={v.icon} title={v.content}>
+              {v.children.map((v) => (
+                <Menu.Item key={v.key} icon={v.icon} title="Hello">
+                  <Link to={`/${v.key}`}>{v.content}</Link>
+                </Menu.Item>
+              ))}
+            </Menu.SubMenu>
+          ) : (
+            <Menu.Item key={v.key} icon={v.icon}>
+              <Link to={`/${v.key}`}>{v.content}</Link>
+            </Menu.Item>
+          ),
+        )}
       </RoutedMenu>
     </Sider>
   )
