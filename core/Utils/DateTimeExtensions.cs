1d51050d752a08c284473236ac2d9ae1e13fdea5
Jannik Buschke <jbu@janniks.work>
2022-06-15 20:10:12 +0200

    feat (Nodatime): add FromUnspecifiedToUtcInstant(DateTime) extension method

--- core/Utils/DateTimeExtensions.cs
+++ core/Utils/DateTimeExtensions.cs
@@ -1,4 +1,6 @@
 using System;
+using NodaTime;
+using NodaTime.Extensions;
 
 namespace Glow.Core.Helpers
 {
@@ -12,6 +14,12 @@ namespace Glow.Core.Helpers
         public static string ToIso8601String(this DateTimeOffset  dateTime)
         {
             return dateTime.ToString("o");
+        }
+
+        public static Instant FromUnspecifiedToUtcInstant(this DateTime dateTime)
+        {
+            var utc = DateTime.SpecifyKind(dateTime, DateTimeKind.Utc);
+            return utc.ToInstant();
         }
     }
 }
