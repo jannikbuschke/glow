5b6c4cda659c9a089f42c02efc4432078a69123a
Jannik <sisosys6@gmail.com>
2022-07-11 15:42:34 +0200

    feat (RestartApplication): restart if AllowRestart is configured

--- glow.beta/Application/RestartApplication.cs
+++ glow.beta/Application/RestartApplication.cs
@@ -10,29 +10,49 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Glow.Core.Application
 {
-    [Action(Policy = DefaultPolicies.Admin, Route = "api/glow/restart-application")]
+    [Action(AllowAnonymous = true, Route = "api/glow/restart-application")]
     public class RestartApplication : IRequest
     {
     }
+
+    public static class IConfigurationExtensions
+    {
+        public static bool AllowRestart(this IConfiguration cfg)
+        {
+            return cfg.GetValue<bool>("AllowConfiguration") == true;
+        }
+    }
+
 
     public class RestartApplicationHandler : IRequestHandler<RestartApplication, Unit>
     {
         private readonly IConfiguration configuration;
         private readonly IApplicationLifetime appLifetime;
+        private readonly ILogger<RestartApplicationHandler> logger;
 
-        public RestartApplicationHandler(IConfiguration configuration, IApplicationLifetime appLifetime)
+        public RestartApplicationHandler(IConfiguration configuration, IApplicationLifetime appLifetime, ILogger<RestartApplicationHandler> logger)
         {
             this.configuration = configuration;
             this.appLifetime = appLifetime;
+            this.logger = logger;
         }
 
         public Task<Unit> Handle(RestartApplication request, CancellationToken cancellationToken)
         {
-            appLifetime.StopApplication();
-            return Unit.Task;
+            if (configuration.AllowRestart())
+            {
+                appLifetime.StopApplication();
+                return Unit.Task;
+            }
+            else
+            {
+                logger.LogInformation("Skip setting openid connect values (AllowConfiguration = false)");
+                throw new BadRequestException("Not possible, editing is not enabled");
+            }
         }
     }
 }
