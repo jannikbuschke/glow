46f3bdc42568ff367b9c63ab1253d9c41dd319c0
Jannik <sisosys6@gmail.com>
2022-07-29 18:59:32 +0200

    feat (JsonSerialization, Meta): add SystemTextJson serialization strategy

--- glow.meta/Startup/ServiceExtensions.cs
+++ glow.meta/Startup/ServiceExtensions.cs
@@ -12,26 +12,32 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
-using Newtonsoft.Json.Serialization;
 using NodaTime;
 using NodaTime.Serialization.JsonNet;
 using IClock = Glow.Clocks.IClock;
+using NodaTime.Serialization.SystemTextJson;
 
 namespace Glow.Core
 {
+    public static class DefaultFsharpJsonSerializationOptions
+    {
+        public const JsonUnionEncoding UnionEncoding = JsonUnionEncoding.AdjacentTag
+                                                       | JsonUnionEncoding.UnwrapRecordCases
+                                                       | JsonUnionEncoding.UnwrapOption
+                                                       | JsonUnionEncoding.UnwrapSingleCaseUnions | JsonUnionEncoding.AllowUnorderedTag;
+    }
+
+    public enum JsonSerializationStrategy
+    {
+        NewtonSoft = 0,
+        SystemTextJson = 1
+    }
+
     public static class ServiceExtensions
     {
-        /// <summary>
-        /// Adds Mvc (with Netwonsoft), FileService, Clock, MockExternalSystems, SpaStaticFiles,
-        /// SignalR, HttpClient, HttpContextAccessor,
-        /// </summary>
-        /// <param name="services"></param>
-        /// <returns></returns>
-        public static IServiceCollection AddGlowApplicationServices(
+        public static IMvcBuilder AddGlowNewtonsoftControllers(
             this IServiceCollection services,
-            Action<MvcOptions> options = null,
-            Action<IMvcBuilder> configureAdditionalMvcOptions = null,
-            params Assembly[] assembliesToScan
+            Action<MvcOptions> options = null
         )
         {
             var mvcBuilder = services.AddControllers(o =>
@@ -52,22 +58,84 @@ namespace Glow.Core
                 {
                     v.SuppressModelStateInvalidFilter = true;
                 });
+            return mvcBuilder;
+        }
+
+        public static IMvcBuilder AddGlowSystemTextJsonControllers(
+            this IServiceCollection services,
+            Action<MvcOptions> options = null
+        )
+        {
+            // let options = JsonSerializerOptions()
+            // options.Converters.Add(JsonFSharpConverter())
+            //
+            // JsonSerializer.Serialize({| x = "Hello"; y = "world!" |}, options)
+
+            var mvcBuilder = services.AddControllers(o =>
+                {
+                    o.EnableEndpointRouting = true;
+                    if (options != null) { options(o); }
+                })
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.ConfigureForNodaTime(DateTimeZoneProviders.Tzdb);
+                    options.JsonSerializerOptions.WriteIndented = true;
+                    // options.JsonSerializerOptions.Form = Formatting.Indented;
+                    // options.JsonSerializerOptions.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                    // options.JsonSerializerOptions.Converters.Add(new StringEnumConverter());
+                    // options.JsonSerializerOptions.Date = DateTimeZoneHandling.Utc;
+
+                    // unionEncoding = (
+                    //     // Base encoding:
+                    //     JsonUnionEncoding.InternalTag
+                    //     // Additional options:
+                    //     ||| JsonUnionEncoding.UnwrapOption
+                    //     ||| JsonUnionEncoding.UnwrapRecordCases
+                    //     )
+
+                    // default: JsonUnionEncoding.AdjacentTag
+                    //     ||| JsonUnionEncoding.UnwrapOption
+                    //     ||| JsonUnionEncoding.UnwrapSingleCaseUnions
+                    //     ||| JsonUnionEncoding.AllowUnorderedTag
+
+                    options.JsonSerializerOptions.Converters.Add(new JsonFSharpConverter(
+                        unionEncoding: DefaultFsharpJsonSerializationOptions.UnionEncoding));
+                })
+                // .AddNewtonsoftJson(options =>
+                // {
+                //     options.SerializerSettings.ConfigureForNodaTime(DateTimeZoneProviders.Tzdb);
+                //     options.SerializerSettings.Formatting = Formatting.Indented;
+                //     options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                //     options.SerializerSettings.Converters.Add(new StringEnumConverter());
+                //     options.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
+                // })
+                .ConfigureApiBehaviorOptions(v =>
+                {
+                    v.SuppressModelStateInvalidFilter = true;
+                });
+            return mvcBuilder;
+        }
+
+        /// <summary>
+        /// Adds Mvc (with Netwonsoft), FileService, Clock, MockExternalSystems, SpaStaticFiles,
+        /// SignalR, HttpClient, HttpContextAccessor,
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddGlowApplicationServices(
+            this IServiceCollection services,
+            Action<MvcOptions> options = null,
+            Action<IMvcBuilder> configureAdditionalMvcOptions = null,
+            JsonSerializationStrategy? useSystemTextJson = JsonSerializationStrategy.NewtonSoft,
+            params Assembly[] assembliesToScan
+        )
+        {
+            var mvcBuilder = useSystemTextJson == JsonSerializationStrategy.NewtonSoft
+                ? services.AddGlowNewtonsoftControllers(options)
+                : services.AddGlowSystemTextJsonControllers(options);
+            configureAdditionalMvcOptions?.Invoke(mvcBuilder);
 
             services.AddGlowActions(assembliesToScan);
-            //
-            // services.AddMvcCore(options =>
-            //     {
-            //         options.Conventions.Add(new ConfigurationControllerRouteConvention());
-            //         options.Conventions.Add(new ActionsControllerRouteConvention());
-            //     })
-            //     .ConfigureApplicationPartManager(m =>
-            //     {
-            //         m.FeatureProviders.Add(new ConfigurationsControllerProvider(assembliesToScan));
-            //         m.FeatureProviders.Add(new ActionsControllerProvider(assembliesToScan));
-            //     })
-            //     .AddApplicationPart(typeof(ActionsControllerProvider).Assembly);
-
-            configureAdditionalMvcOptions?.Invoke(mvcBuilder);
 
             services.AddGlow();
             services.AddSpaStaticFiles(configuration =>
