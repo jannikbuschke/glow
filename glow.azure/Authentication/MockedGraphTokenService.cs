fadae5db6a8b4b6f40460c44e14bfa6fb71b0f06
Jannik <sisosys6@gmail.com>
2022-08-08 12:44:05 +0200

    feat (MockedGraphTokenService): use provided userid and cache tokens

--- glow.azure/Authentication/MockedGraphTokenService.cs
+++ glow.azure/Authentication/MockedGraphTokenService.cs
@@ -9,6 +9,7 @@ using Glow.TestAutomation;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Primitives;
 using Microsoft.Graph;
@@ -27,20 +28,23 @@ namespace Glow.Core.Authentication
         private readonly IConfiguration configuration;
         private readonly IOptions<AadFakeAuthenticationOptions> fakeAuthOptions;
         private readonly IOptionsSnapshot<AzureAdOptions> aadOptions;
+        private readonly ILogger<MockedGraphTokenService> logger;
+        private static System.Collections.Generic.Dictionary<string, AuthenticationResult> Cache = new();
 
         public MockedGraphTokenService(
             IHttpContextAccessor httpContextAccessor,
             IHttpClientFactory clientFactory,
             IConfiguration configuration,
             IOptions<AadFakeAuthenticationOptions> fakeAuthOptions,
-            IOptionsSnapshot<AzureAdOptions> aadOptions
-        )
+            IOptionsSnapshot<AzureAdOptions> aadOptions,
+            ILogger<MockedGraphTokenService> logger)
         {
             this.httpContextAccessor = httpContextAccessor;
             this.clientFactory = clientFactory;
             this.configuration = configuration;
             this.fakeAuthOptions = fakeAuthOptions;
             this.aadOptions = aadOptions;
+            this.logger = logger;
         }
 
         public override Task<string> AccessTokenForApp()
@@ -84,39 +88,61 @@ namespace Glow.Core.Authentication
             StringValues? usernames = httpContextAccessor?.HttpContext?.Request?.Headers["x-username"];
             var username = usernames?.FirstOrDefault() ?? fakeAuthOptions.Value.DefaultUserName;
 
-            FakeUser? user = id == null && username == null
-                ? null //fakeAuthOptions.Value.Users?.FirstOrDefault(v => v.UserName == fakeAuthOptions.Value.DefaultUserName)
-                : fakeAuthOptions.Value.Users?.FirstOrDefault(v => v.UserName == username);
+            FakeUser? user = id != null
+                ? fakeAuthOptions.Value.Users?.FirstOrDefault(v => v.UserName == id)
+                : username == null
+                    ? null //fakeAuthOptions.Value.Users?.FirstOrDefault(v => v.UserName == fakeAuthOptions.Value.DefaultUserName)
+                    : fakeAuthOptions.Value.Users?.FirstOrDefault(v => v.UserName == username);
 
             if (user == null)
             {
+                logger.LogInformation("No user found");
                 return null;
             }
 
-            AzureAdOptions aad = aadOptions.Value;
-            var tenantId = aad.TenantId;
-            var clientId = aad.ClientId;
-            var clientSecret = aad.ClientSecret;
+            var isExpired = Cache.ContainsKey(user.UserName) && Cache[user.UserName].ExpiresOn < DateTimeOffset.UtcNow.AddMinutes(60);
+            if (!Cache.ContainsKey(user.UserName) || isExpired)
+            {
+                if (isExpired)
+                {
+                    logger.LogInformation($"Token expired, refresh");
+                }
+                try
+                {
+                    logger.LogInformation($"User {user.UserName} not yet cached");
+                    AzureAdOptions aad = aadOptions.Value;
+                    var tenantId = aad.TenantId;
+                    var clientId = aad.ClientId;
+                    var clientSecret = aad.ClientSecret;
 
-            HttpClient client = clientFactory.CreateClient();
-            PasswordFlow.AccessTokenResponse result = await client.GetMsAccessTokentWithUsernamePassword(
-                clientSecret,
-                user.UserName,
-                user.Password,
-                clientId,
-                tenantId);
+                    HttpClient client = clientFactory.CreateClient();
+                    PasswordFlow.AccessTokenResponse result = await client.GetMsAccessTokentWithUsernamePassword(
+                        clientSecret,
+                        user.UserName,
+                        user.Password,
+                        clientId,
+                        tenantId);
 
-            return new AuthenticationResult(
-                accessToken: result.access_token,
-                isExtendedLifeTimeToken: true,
-                uniqueId: Guid.NewGuid().ToString(),
-                expiresOn: DateTimeOffset.Now,
-                extendedExpiresOn: DateTimeOffset.Now,
-                tenantId: tenantId,
-                account: null,
-                idToken: null,
-                scopes: scope,
-                correlationId: Guid.NewGuid());
+                    Cache[user.UserName] = new AuthenticationResult(
+                        accessToken: result.access_token,
+                        isExtendedLifeTimeToken: false,
+                        uniqueId: Guid.NewGuid().ToString(),
+                        expiresOn: DateTimeOffset.UtcNow.AddSeconds(result.expires_in),
+                        extendedExpiresOn: DateTimeOffset.UtcNow.AddSeconds(result.ext_expires_in),
+                        tenantId: tenantId,
+                        account: null,
+                        idToken: null,
+                        scopes: scope,
+                        correlationId: Guid.NewGuid());
+                }
+                catch (Exception e)
+                {
+                    logger.LogError(e, "Could not authenticate " + user.UserName);
+                    return null;
+                }
+            }
+
+            return Cache[user.UserName];
         }
     }
 }
