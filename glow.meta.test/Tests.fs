d960eb160b37c74110844cea607fea5163ce3607
Jannik <sisosys6@gmail.com>
2022-09-21 14:04:43 +0200

    start working on fs ts gen

--- glow.meta.test/Tests.fs
+++ glow.meta.test/Tests.fs
@@ -1,231 +1,124 @@
 namespace GlowTest
 
 open System
-open System.Globalization
 open Expecto
-open System.Linq
-open Glow.Core.Typescript
+open Glow.TsGen
 open Xunit
 
 module Tests =
 
   type SimpleRecord = { Id: Guid; Name: string; Number: int }
 
+  [<Fact>]
+  let ``Render simple record`` () =
+    let types = [ typedefof<SimpleRecord> ]
+
+    let modules =
+      Glow.TsGen.generateModules types
+
+    Expect.hasLength modules 1 "Module count"
+
+    Expect.hasLength modules.Head.Items 1 "Item count"
+
+    Expect.isEmpty modules.Head.Items.Head.Dependencies "No deps"
+
+    let rendered = Glow.TsGen.renderType modules.Head.Items.Head
+
+    "Rendered ts type as expected" |> Expect.equal rendered """
+export type SimpleRecord = {
+  id: Guid
+  name: String
+  number: Int32
+}"""
+
   type RecordWithPrimitiveOption = { Id: Guid; NumberOption: int option }
+
+  [<Fact>]
+  let ``Render record with primitive option`` () =
+    let types =
+      [ typedefof<RecordWithPrimitiveOption> ]
+
+    let modules =
+      Glow.TsGen.generateModules types
+
+    Expect.hasLength modules 3 "Module count"
+
+    let fsharpCore = modules |> List.find (fun v -> v.Name = TsNamespace("Microsoft.FSharp.Core"))
+    let system = modules |> List.find (fun v -> v.Name = TsNamespace("System"))
+    let glowTest = modules |> List.find (fun v -> v.Name = TsNamespace("GlowTest"))
+
+    "Item count fsharp" |> Expect.hasLength fsharpCore.Items 1
+    "Item count system" |> Expect.hasLength system.Items 3
+    "Item count glow" |> Expect.hasLength glowTest.Items 1
+
+    let fsharpCoreRendered = Glow.TsGen.renderType fsharpCore.Items.Head
+
+    Expect.hasLength modules.Head.Items 1 "Item count"
+
+    "Rendered FSharpOption as expected" |> Expect.equal fsharpCoreRendered """
+export type FSharpOption<T> = T | null
+"""
+
+    let systemRendered = system.Items |> List.map Glow.TsGen.renderType |> String.concat "\n"
+
+    "Rendered System types as expected" |> Expect.equal systemRendered """export type Guid = string
+export type Int32 = number
+export type Boolean = bool"""
+
+    Expect.isEmpty modules.Head.Items.Head.Dependencies "No deps"
+
+    let rendered = Glow.TsGen.renderType glowTest.Items.Head
+
+    "Rendered glow type as expected" |> Expect.equal rendered """
+export type RecordWithPrimitiveOption = {
+  id: Guid
+  numberOption: FSharpOption<Int32>
+}"""
+
+  type RecordWithOption =
+    { Id: Guid
+      Option: SimpleRecord option }
+
+  type RecordWithResult =
+    { Id: Guid
+      Result: Result<int, string> }
+
+  type RecordWithResult2 =
+    { Id: Guid
+      Result: Result<SimpleRecord, string> }
 
   type RecordWithNodatimeInstant = { Id: Guid; Instant: NodaTime.Instant }
 
   type RecordWithPrimitiveList = { Id: Guid; StringList: string list }
 
   type RecordWithRecordList =
-      { Id: Guid
-        RecordList: SimpleRecord list }
+    { Id: Guid
+      RecordList: SimpleRecord list }
 
   type RecordWithOptionalRecordList =
-      { Id: Guid
-        OptionalRecordList: SimpleRecord option list }
+    { Id: Guid
+      OptionalRecordList: SimpleRecord option list }
 
   type SingleCaseUnion = Email of string
   type RecordWithSingleCaseDu = { SingleCaseUnion: SingleCaseUnion }
 
   type DuWithoutTypes =
-      | FirstCase
-      | SecondCase
-      | ThirdCase
+    | FirstCase
+    | SecondCase
+    | ThirdCase
 
   type ComplexDu =
-      | Record of SimpleRecord
-      | Record2 of RecordWithPrimitiveOption
+    | Record of SimpleRecord
+    | Record2 of RecordWithPrimitiveOption
 
   type MixedDu =
-      | FirstCase
-      | SecondCase of string
-      | Record of SimpleRecord
-      | Record2 of RecordWithPrimitiveOption
+    | FirstCase
+    | SecondCase of string
+    | Record of SimpleRecord
+    | Record2 of RecordWithPrimitiveOption
 
   type RecordWithDuWithoutFields = { Id: Guid; Du: DuWithoutTypes }
 
   type RecordWithComplexDu = { Id: Guid; Du: ComplexDu }
 
   type RecordWithMixedDu = { Id: Guid; Du: MixedDu }
-
-
-  [<Fact>]
-  let ``Render primitive option as nullable`` () =
-    let builder = TypeCollectionBuilder()
-    builder.Add<RecordWithPrimitiveOption>()
-    let collection = builder.Generate(null)
-    let m = collection.Modules.First()
-
-    let renderedModule =
-      RenderTypes.RenderModule(m, null)
-    let expected =
-      @"export interface RecordWithPrimitiveOption {
-  id: string
-  numberOption: number | null
-}
-
-export const defaultRecordWithPrimitiveOption: RecordWithPrimitiveOption = {
-  id: ""00000000-0000-0000-0000-000000000000"",
-  numberOption: null,
-}
-
-
-"
-
-    let compareResult = String.Compare(renderedModule, expected, CultureInfo.InvariantCulture, CompareOptions.IgnoreCase ||| CompareOptions.IgnoreSymbols)
-
-    Expect.equal compareResult 0 "compare"
-
-
-
-  [<Fact>]
-  let ``Render Single case union`` () =
-    let builder = TypeCollectionBuilder()
-    builder.Add<RecordWithSingleCaseDu>()
-    let collection = builder.Generate(null)
-    let m = collection.Modules.First()
-
-    let renderedModule =
-      RenderTypes.RenderModule(m, null)
-    let expected =
-      @"export interface RecordWithSingleCaseDu {
-  singleCaseUnion: string | null
-}
-
-export const defaultRecordWithSingleCaseDu: RecordWithSingleCaseDu = {
-  singleCaseUnion: null,
-}
-
-
-
-"
-
-    let compareResult = String.Compare(renderedModule, expected, CultureInfo.InvariantCulture, CompareOptions.IgnoreCase ||| CompareOptions.IgnoreSymbols)
-
-    Expect.equal compareResult 0 "compare"
-
-
-  [<Fact>]
-  let ``Render ComplexDu`` () =
-    let builder = TypeCollectionBuilder()
-    builder.Add<RecordWithComplexDu>()
-    let collection = builder.Generate(null)
-    let m = collection.Modules.First()
-
-    let renderedModule =
-      RenderTypes.RenderModule(m, null)
-    let expected =
-      @"export interface SimpleRecord {
-}
-
-export const defaultSimpleRecord: SimpleRecord = {
-  id: null,
-}
-
-export interface RecordWithPrimitiveOption {
-}
-
-export const defaultRecordWithPrimitiveOption: SimpleRecord = {
-  id: null,
-}
-
-type RecordCase = {
-  ""Case"":""Record"",
-  Fields: SimpleRecord
-}
-
-type Record2Case = {
-  ""Case"":""Record2"",
-  Fields: RecordWithPrimitiveOption
-}
-
-type ComplexDu = RecordCase | Record2Case
-
-export const defaultRecordWithSingleCaseDu: RecordWithSingleCaseDu = {
-  id: null,
-}
-
-interface RecordWithComplexDu = {
-  Id: string
-  Du: ComplexDu
-}
-
-
-"
-
-    let compareResult = String.Compare(renderedModule, expected, CultureInfo.InvariantCulture, CompareOptions.IgnoreCase ||| CompareOptions.IgnoreSymbols)
-
-    Expect.equal compareResult 0 "compare"
-
-
-  [<Fact>]
-  let ``Render Du without fields`` () =
-    let builder = TypeCollectionBuilder()
-    builder.Add<RecordWithDuWithoutFields>()
-    let collection = builder.Generate(null)
-    let m = collection.Modules.First()
-
-    let renderedModule =
-      RenderTypes.RenderModule(m, null)
-    let expected =
-      @"export type DuWithoutTypes_Case_FirstCase = {
-  case: ""FirstCase""
-}
-
-export type DuWithoutTypes_Case_SecondCase = {
-  case: ""SecondCase""
-}
-
-export type DuWithoutTypes_Case_ThirdCase = {
-  case: ""ThirdCase""
-}
-
-
-export type DuWithoutTypes = DuWithoutTypes_Case_FirstCase | DuWithoutTypes_Case_SecondCase | DuWithoutTypes_Case_ThirdCase
-
-
-export const defaultDuWithoutTypes = null as any as DuWithoutTypes
-
-
-"
-
-    let compareResult = String.Compare(renderedModule, expected, CultureInfo.InvariantCulture, CompareOptions.IgnoreCase ||| CompareOptions.IgnoreSymbols)
-
-    Expect.equal compareResult 0 "compare"
-
-
-  [<Fact>]
-  let ``Render Du with mixed cases`` () =
-    let builder = TypeCollectionBuilder()
-    builder.Add<RecordWithMixedDu>()
-    let collection = builder.Generate(null)
-    let m = collection.Modules.First()
-
-    let renderedModule =
-      RenderTypes.RenderModule(m, null)
-    let expected =
-      @"export type DuWithoutTypes_Case_FirstCase = {
-  case: ""FirstCase""
-}
-
-export type DuWithoutTypes_Case_SecondCase = {
-  case: ""SecondCase""
-}
-
-export type DuWithoutTypes_Case_ThirdCase = {
-  case: ""ThirdCase""
-}
-
-
-export type DuWithoutTypes = DuWithoutTypes_Case_FirstCase | DuWithoutTypes_Case_SecondCase | DuWithoutTypes_Case_ThirdCase
-
-
-export const defaultDuWithoutTypes = null as any as DuWithoutTypes
-
-
-"
-
-    let compareResult = String.Compare(renderedModule, expected, CultureInfo.InvariantCulture, CompareOptions.IgnoreCase ||| CompareOptions.IgnoreSymbols)
-
-    Expect.equal compareResult 0 "compare"
---
+++ glow.ts.fs/GenerateModules.fs
@@ -0,0 +1,215 @@
+﻿namespace Glow
+
+open System
+
+module TsGen =
+
+  type ModuleName = ModuleName of string
+  type NamespaceName = NamespaceName of string
+
+  type TsTypeId = TsTypeId of string
+
+  type TsNamespace = TsNamespace of string
+  type TsName = TsName of string
+  type GenericArguments = string list
+
+  type TsSignature =
+    { TsNamespace: TsNamespace
+      TsName: TsName
+      GenericArgumentTypes: TsSignature list
+      GenericArguments: GenericArguments }
+    member this.IsGeneric() = this.GenericArguments.Length > 0
+    member this.GetName()=
+      let (TsName name)=this.TsName
+      name
+
+    member this.NameWithGenericArguments() =
+      let args =
+        "<"
+        + (this.GenericArgumentTypes
+           |> Seq.map (fun v -> v.TsName)
+           |> Seq.map (fun (TsName name) -> name)
+           |> String.concat ",")
+        + ">"
+
+      let empty = ""
+      $"{this.GetName()}{(if this.IsGeneric() then args else empty)}"
+
+  type FullTsTypeId =
+    { Id: TsTypeId
+      OriginalName: string
+      OriginalNamespace: NamespaceName
+      TsSignature: TsSignature }
+
+  let rec getTsSignature (t: Type) : TsSignature =
+
+    let args0 = t.GetGenericArguments()
+
+    let args =
+      t.GetGenericArguments()
+      |> Seq.toList
+      |> List.mapi (fun i v -> $"T{i}")
+
+    let name =
+      if args.Length > 0 then
+        t.Name.Substring(0, (t.Name.IndexOf "`"))
+      else
+        t.Name
+
+    let genericArgumentTypes =
+      if args.Length > 0 then
+        args0
+        |> Seq.toList
+        |> List.map (fun v -> getTsSignature (v))
+      else
+        []
+
+    match t.Namespace, t.Name with
+    // | "Microsoft.FSharp.Core", "FSharpOption`1" -> { TsNamespace = TsNamespace t.Namespace; TsName = TsName name; GenericArguments = args }
+    //
+    // | "System", "Guid" -> { TsNamespace = TsNamespace t.Namespace; TsName = TsName t.Name; GenericArguments = args }
+    // | "System", "Int32" -> { TsNamespace = TsNamespace t.Namespace; TsName = TsName t.Name; GenericArguments = args }
+    // | "System", "Boolean" -> { TsNamespace = TsNamespace t.Namespace; TsName = TsName t.Name; GenericArguments = args }
+    | _ ->
+      { TsNamespace = TsNamespace t.Namespace
+        TsName = TsName name
+        GenericArgumentTypes = genericArgumentTypes
+        GenericArguments = args }
+
+  let getModuleNameAndId (arg: Type) : FullTsTypeId =
+    { Id = TsTypeId arg.FullName
+      TsSignature = getTsSignature arg
+      OriginalName = arg.Name
+      OriginalNamespace = NamespaceName arg.Namespace }
+
+  type TsType =
+    { Id: FullTsTypeId
+      Type: Type
+      Dependencies: FullTsTypeId list }
+
+  type Namespace =
+    { Name: TsNamespace
+      Items: TsType list }
+
+  let rec toTsType (depth: int) (t: Type) : TsType list =
+
+    let properties =
+      t.GetProperties()
+      |> Seq.toList
+      |> List.collect (fun v ->
+        if v.PropertyType.Equals(t) then
+          []
+        else
+          toTsType (depth + 1) v.PropertyType)
+      |> List.distinct
+
+    let result =
+      if t.IsGenericType then
+        let genericArgs = t.GetGenericArguments()
+
+        let tsArgs =
+          genericArgs
+          |> Seq.toList
+          |> List.collect (toTsType (depth + 1))
+
+        let fullId = getModuleNameAndId t
+
+        ({ Id = fullId
+           Type = t
+           Dependencies = [] }
+         :: tsArgs)
+        @ properties
+      else
+        let fullId = getModuleNameAndId t
+
+        [ { Id = fullId
+            Type = t
+            Dependencies = [] } ]
+        @ properties
+
+    result
+
+  let generateModules (types: Type list) : Namespace list =
+    let result =
+      types |> List.collect (toTsType 0)
+
+    let modules =
+      result
+      |> List.groupBy (fun v -> v.Id.TsSignature.TsNamespace)
+      |> List.map (fun (namespaceName, types) ->
+        { Name = namespaceName
+          Items = types |> Seq.distinct |> Seq.toList })
+
+    modules
+
+  let camelize (arg: string) : string =
+    System.Text.Json.JsonNamingPolicy.CamelCase.ConvertName(arg)
+
+  let renderProperties (t: TsType) : string =
+    let props = t.Type.GetProperties()
+
+    let result =
+      props
+      |> Seq.toList
+      |> List.map (fun v ->
+        let tsType = toTsType 0 v.PropertyType
+        let signature = tsType.Head.Id.TsSignature
+
+        if signature.IsGeneric() then
+          let name =
+            signature.NameWithGenericArguments()
+
+          $"{camelize v.Name}: {name}"
+        else
+          $"{camelize v.Name}: {v.PropertyType.Name}")
+      |> String.concat "\n  "
+
+    result
+
+  let renderType (t: TsType) : string =
+    match t.Type.Namespace, t.Type.Name with
+    | "Microsoft.FSharp.Core", "FSharpOption`1" ->
+      """
+export type FSharpOption<T> = T | null
+"""
+    | "System", "Guid" -> "export type Guid = string"
+    | "System", "Int32" -> "export type Int32 = number"
+    | "System", "Boolean" -> "export type Boolean = bool"
+    | _ ->
+      $"""
+export type {t.Id.OriginalName} = {{
+  {renderProperties t}
+}}"""
+
+  let renderModule (m: Namespace) : string =
+    // topological sort
+    let deps =
+      m.Items
+      |> List.collect (fun v -> v.Dependencies)
+      |> List.filter (fun v -> v.TsSignature.TsNamespace <> m.Name)
+
+    ""
+
+  let FsharpResult =
+    """
+
+  type Ok<T> = {
+    Case: "Ok"
+    Fields: T
+  }
+
+  type Error<T> = {
+    Case: "Error"
+    Fields: T
+  }
+
+  type Result = Ok<TOk> | Error<TError>
+
+  """
+
+  let FsharpOption =
+    """
+
+  type Option<T> = T | null
+
+  """
