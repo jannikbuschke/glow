3eb58f809b8cdfb23ddae357239e0e08456d8c70
Jannik <sisosys6@gmail.com>
2022-12-05 13:25:04 +0100

    feat (TsGen): allow providing a path

--- glow.core.fs/GenerateTsClient.fs
+++ glow.core.fs/GenerateTsClient.fs
@@ -3,9 +3,12 @@
 open System.Reflection
 open Glow.TsGen.Domain
 
-let renderTsTypes (assemblies: Assembly list) =
+let renderTsTypesInternal (path: string) (assemblies: Assembly list) =
   printfn "Generate ts types"
 
+  if not (System.IO.Directory.Exists(path)) then
+    System.IO.Directory.CreateDirectory path |> ignore
+    ()
   let stopWatch = System.Diagnostics.Stopwatch.StartNew()
   let es = GetTypes.getEvents assemblies
   let actions = GetTypes.getRequests assemblies
@@ -22,18 +25,24 @@ let renderTsTypes (assemblies: Assembly list) =
   let modules =
     Glow.TsGen.Gen.generateModules allTypes
 
+
   modules
   |> List.iter (fun v ->
     let fs = Glow.TsGen.Gen.renderModule v
-    System.IO.File.WriteAllText($".\\web\\src\\client\\{NamespaceName.filename v.Name}", fs)
+    System.IO.File.WriteAllText($"{path}{NamespaceName.filename v.Name}", fs)
     ()
   )
 
-  GenerateApi.render assemblies $".\\web\\src\\client\\"
-  GenerateSubscriptions.render assemblies $".\\web\\src\\client\\"
+  GenerateApi.render assemblies path
+  GenerateSubscriptions.render assemblies path
   stopWatch.Stop()
   printfn "Generated time in %f ms" stopWatch.Elapsed.TotalMilliseconds
   ()
 
 // RenderApi.Render(typeCollection, option);
 // RenderSubscrptions.Render(typeCollection, option);
+let renderTsTypes (assemblies: Assembly list) =
+  renderTsTypesInternal ".\\web\\src\\client\\" assemblies
+
+let renderTsTypesFromAssemblies (assemblies: Assembly seq) (path:string) =
+  assemblies |> Seq.toList |> renderTsTypesInternal path
