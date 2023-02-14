862c5de8fd63e9ea21636f66ab3aa015ca86adc7
Jannik <sisosys6@gmail.com>
2023-02-14 15:31:05 +0100

    update sample data generator

--- glow.ts.fs/SampleData.fs
+++ glow.ts.fs/SampleData.fs
@@ -5,27 +5,55 @@ open System.Reflection
 open Glow.Ts
 
 type SampleData =
-  abstract Create: unit -> obj
+  abstract Create: unit -> (string * obj) list
 
-module Module =
+module SampleData =
   let findTypes (assembly: Assembly) =
     let t = typeof<SampleData>
 
-    assembly.GetTypes() |> Seq.filter t.IsAssignableFrom
+    assembly.GetTypes()
+    |> Seq.filter t.IsAssignableFrom
 
   let instantiateSampleObjects (t: Type) =
     let sampleData = Activator.CreateInstance(t)
     let sampleData = sampleData :?> SampleData
     sampleData.Create()
 
-  let render (serialize: Serialize) (obj: obj) = serialize obj
+  let render (serialize: Serialize) (data: (string * obj) list) =
+    let result =
+      data
+      |> List.map (fun (name, obj) ->
+        let typeName = Glow.SecondApproach.getName (obj.GetType())
+        let moduleName = Glow.SecondApproach.getModuleName (obj.GetType())
+        let propSignature = Glow.SecondApproach.getPropertySignature "" (obj.GetType())
+        let importName = $"import * as {moduleName} from \"./{moduleName}\""
+        importName, $""""{name}": {serialize obj} as {propSignature}""")
+
+    result
 
 let generateSampleData (path: string) (assemblies: Assembly seq) (serialize: Serialize) =
-  let text =
+  let imports, definitions =
     assemblies
-    |> Seq.collect Module.findTypes
-    |> Seq.map Module.instantiateSampleObjects
-    |> Seq.map (Module.render serialize)
-    |> String.concat "\n"
+    |> Seq.collect SampleData.findTypes
+    |> Seq.map SampleData.instantiateSampleObjects
+    |> Seq.map (SampleData.render serialize)
+    |> (fun x ->
+      let imports =
+        x
+        |> Seq.collect (fun v -> v |> Seq.map fst)
+        |> Seq.distinct
+        |> String.concat "\n"
 
-  System.IO.File.WriteAllText(path, $"""export const sample_data = {text} as const""")
+      let definitions =
+        x
+        |> Seq.collect (fun v -> v |> Seq.map snd)
+        |> String.concat ",\n"
+
+      imports, definitions)
+
+  System.IO.File.WriteAllText(
+    path,
+    $"""{(imports)}
+
+export const sample_data = {{ {definitions} }}"""
+  )
